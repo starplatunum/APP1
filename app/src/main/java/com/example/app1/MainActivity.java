eb5dacbbc633d399da1165bb9c181e8181ac7494
starplatunum <1304853581@qq.com>
2020-09-15 23:12:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button1);
+    }
+
+    public void btn(View view) {
+    }
+}
